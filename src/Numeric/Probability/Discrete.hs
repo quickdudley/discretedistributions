e723af1e3caaef681eba2a0af6f4d481cd78e21e
Jeremy List <quick.dudley@gmail.com>
2020-01-03 12:57:03 +1300

    Num and Show instances

--- src/Numeric/Probability/Discrete.hs
+++ src/Numeric/Probability/Discrete.hs
@@ -28,6 +28,18 @@ instance Monad Distribution where
   return = pure
   P a >>= f = P [(pa * pb,b) | ~(pa,a') <- a, ~(pb,b) <- runDistribution (f a')]
 
+instance Show a => Show (Distribution a) where
+  showsPrec _ (P d) = ("proportional "++) . showsPrec 11 d
+
+instance (Ord n, Num n) => Num (Distribution n) where
+  a + b = normalize $ (+) <$> a <*> b
+  a * b = normalize $ (*) <$> a <*> b
+  a - b = normalize $ (-) <$> a <*> b
+  negate = fmap negate
+  abs = fmap abs
+  signum = fmap signum
+  fromInteger = pure . fromInteger
+
 -- | Creates a distribution with the probability of each outcome being
 -- proportional to the given weight.
 proportional :: Real p => [(p,a)] -> Distribution a
